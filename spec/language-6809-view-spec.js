957a705b0ed91a710d24469335230eba0a2df326
Georg Ziegler <georgjziegler@gmail.com>
2018-01-09 00:10:13 +0100

    First commit

---
+++ spec/language-6809-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import Language6809View from '../lib/language-6809-view';
+
+describe('Language6809View', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
